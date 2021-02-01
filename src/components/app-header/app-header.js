5aab691208ac54b983739d5afef053b2c5e9889b
OlegEf <5tg6yy@gmuil.com>
2021-02-01 13:25:00 +0300

    add folder components /src/components/app-header, component AppHeader

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './app-header.css';
+
+const AppHeader = ({toDo, done}) => {
+  return (
+    <div id="header">
+            <div id="top-header">
+                <div id="logo">
+                    <h1>
+                        EOV
+                    </h1>
+                </div>                
+                <div id="social-icons">
+					<ul>
+						<li><a href="https://m.facebook.com/home.php"></a>l</li>
+						<li><a href="https://m.facebook.com/home.php"></a>f</li>
+						<li><a href="https://m.facebook.com/home.php"></a>d</li>
+						<li><a href="https://m.facebook.com/home.php"></a>r</li>				
+					</ul>
+                </div>
+            </div>
+            <div id="many">
+                    <div class="manya">
+						<h1>Персональный сайт EOV</h1>
+						<h2>
+                            автор: Олег Ефимчук
+                        </h2>
+                    </div>
+                    <div class="manyb">
+						<h3>проект EOV на гитхаб</h3>
+                    </div>
+					<p class="autor"><a href="https://github.com/OlegEf/Entry-assignment-to-internship-2020" target="blank">https://github.com/OlegEf/</a></p>
+            </div>
+		</div>
+  );
+};
+
+export default AppHeader;
