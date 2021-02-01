235d9093c386f706a83aeca242d7d4233641e606
OlegEf <5tg6yy@gmuil.com>
2021-02-01 13:21:47 +0300

    add folder component /src/components/app, component App

---
+++ src/components/app/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+
+import './app.css';
+
+const App = () => {
+
+  return (
+	<body>
+		<div class="container">
+
+		  <AppHeader />
+		</div>
+	</body>
+  );
+};
+
+export default App;
