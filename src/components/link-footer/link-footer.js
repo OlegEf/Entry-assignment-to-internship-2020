37f1a29a11a76478c5816d45c83d65ec984e46df
OlegEf <5tg6yy@gmuil.com>
2021-02-05 22:46:22 +0300

    added link-footer.js/const demoLink str.5,13

--- src/components/link-footer/link-footer.js
+++ src/components/link-footer/link-footer.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import './link-footer.css';
 
+const demoLink = ("https://p.w3layouts.com/demos/metushi/web/");
+
 const LinkFooter = () => {
   return (
 			<div className="row">
 				<a href="#footer">быстрый переход footer</a>
 					<h3 className="demo">Данный проект демонстрирует  базовые возможности html css</h3>
 					<span>ссылка на оригинал дизайна</span>
-					<p className="demo"><a href="https://p.w3layouts.com/demos/metushi/web/">https://p.w3layouts.com/demos/metushi/web/</a></p>
+					<p class="demo"><a href={demoLink}>{demoLink}</a></p>
             </div>
   );
 };
